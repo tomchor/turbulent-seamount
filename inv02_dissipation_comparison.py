56d3c17885fc566d5a6796e05d9a8a78bf9a6f0e
tomchor <tomaschor@gmail.com>
2025-02-03 14:42:34 -0700

    better inv02

--- inv02_dissipation_comparison.py
+++ inv02_dissipation_comparison.py
@@ -14,14 +14,15 @@ plt.rcParams["font.size"] = 9
 π = np.pi
 
 path = "simulations/data/"
-modifiers = ["-f4", ""]
+modifiers = ["-A02-f4", "-A02"]
 #modifiers = ["-f8", "-f4",]
 alphas = (0.3, 1)
 normalized_offsets = (-1/2, 0, 1/2)
 V_tokara = 1 # m/s
 H_tokara = 500 # meters
 
-fig, ax = plt.subplots(constrained_layout=True, sharey=True, figsize=(10, 4))
+fig, axes = plt.subplots(nrows=3, constrained_layout=True, figsize=(10, 8),
+                         sharex=True)
 
 for j, modifier in enumerate(modifiers):
     #+++ Open dataset and pick time
@@ -41,9 +42,15 @@ for j, modifier in enumerate(modifiers):
 
     #+++ Take vertical average
     xyz["ε̄ₖ"]  = (xyz["εₖ"]  * xyz["Δzᶜᶜᶜ"]).pnsum("z") / xyz["Δzᶜᶜᶜ"].pnsum("z")
+    xyz["ε̄ₚ"]  = (xyz["εₚ"]  * xyz["Δzᶜᶜᶜ"]).pnsum("z") / xyz["Δzᶜᶜᶜ"].pnsum("z")
 
     xyz["ℱεₖ"] = xyz["εₖ"].where(xyz.altitude > 4)
+    xyz["ℱεₚ"] = xyz["εₚ"].where(xyz.altitude > 4)
+
     xyz["ℱε̄ₖ"] = (xyz["ℱεₖ"] * xyz["Δzᶜᶜᶜ"]).pnsum("z") / xyz["Δzᶜᶜᶜ"].pnsum("z")
+    xyz["ℱε̄ₚ"] = (xyz["ℱεₚ"] * xyz["Δzᶜᶜᶜ"]).pnsum("z") / xyz["Δzᶜᶜᶜ"].pnsum("z")
+
+    xyz["γ"] = xyz["ℱε̄ₚ"] / (xyz["ℱε̄ₚ"] + xyz["ℱε̄ₖ"])
     #---
 
     #+++ Upscale LES results
@@ -53,21 +60,32 @@ for j, modifier in enumerate(modifiers):
 
     xyz["ε̄ₖ_upscaled"]  = xyz["ε̄ₖ"]  * ℰₖ_tokara / ℰₖ_LES
     xyz["ℱε̄ₖ_upscaled"] = xyz["ℱε̄ₖ"] * ℰₖ_tokara / ℰₖ_LES
+
+    xyz["ε̄ₚ_upscaled"]  = xyz["ε̄ₚ"]  * ℰₖ_tokara / ℰₖ_LES
+    xyz["ℱε̄ₚ_upscaled"] = xyz["ℱε̄ₚ"] * ℰₖ_tokara / ℰₖ_LES
     #---
 
     alpha = alphas[j]
     for i, offset in enumerate(normalized_offsets):
-        label = f"Filtered Vert avg εₖ @ {offset:.1f} FWMH" if j==0 else ""
         print(f"Plotting {j}-th modifier = {modifier}, {i}-th offset = {offset}")
         color = plt.rcParams["axes.prop_cycle"].by_key()["color"][i]
-
         xyz_line = xyz.sel(xC=offset*xyz.FWMH, method="nearest")
-        xyz_line["ℱε̄ₖ_upscaled"].plot(ax=ax, label=label, color=color, alpha=alpha)
 
-ax.set_ylim(1e-9, 1e-5)
-ax.set_yscale("log")
-ax.set_title("")
-ax.legend()
-ax.grid(True)
+        label = f"Filtered Vert avg εₖ @ {offset:.1f} FWMH" if j==0 else ""
+        xyz_line["ℱε̄ₖ_upscaled"].plot(ax=axes[0], label=label, color=color, alpha=alpha)
+
+        label = f"Filtered Vert avg εₚ @ {offset:.1f} FWMH" if j==0 else ""
+        xyz_line["ℱε̄ₚ_upscaled"].plot(ax=axes[1], label=label, color=color, alpha=alpha)
+
+        label = f"Mixing efficiency @ {offset:.1f} FWMH" if j==0 else ""
+        xyz_line["γ"].plot(ax=axes[2], label=label, color=color, alpha=alpha)
+
+for i, ax in enumerate(axes):
+    if i < 2:
+        ax.set_ylim(1e-9, 1e-5)
+        ax.set_yscale("log")
+    ax.set_title("")
+    ax.legend()
+    ax.grid(True)
 
 fig.savefig(f"figures/dissipation_comparison.png")

1f0d7e1c6e95b32e5aa9702f1a60a6bae958bba6
tomchor <tomaschor@gmail.com>
2025-02-10 19:33:11 -0700

    go back to original params

--- simulations/run_all_simulations_pbs.py
+++ simulations/run_all_simulations_pbs.py
@@ -7,12 +7,11 @@ from aux00_utils import aggregate_parameters
 #+++ Define run options
 simname_base = "tokara"
 
-#resolutions = cycler(res = [8, 4, 2, 1])
-resolutions = cycler(res = [8, 4, 2])
+resolutions = cycler(res = [8, 4, 2, 1])
 slopes = cycler(α = [0.05, 0.1, 0.2])
 slopes = cycler(α = [0.2,])
-Rossby_numbers = cycler(Ro_h = [0.08, 0.2, 0.5, 1.25])
-Froude_numbers = cycler(Fr_h = [0.08, 0.2, 0.5, 1.25])
+Rossby_numbers = cycler(Ro_h = [1.4])
+Froude_numbers = cycler(Fr_h = [0.6])
 runs = resolutions * slopes * Rossby_numbers * Froude_numbers
 #---
 

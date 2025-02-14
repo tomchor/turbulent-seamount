cfe2d6b4efaf6ecf6f992ca0ab70fc6100b9f315
tomchor <tomaschor@gmail.com>
2025-02-13 18:46:18 -0700

    improve naming order of the runs

--- 00_run_postproc.py
+++ 00_run_postproc.py
@@ -5,7 +5,7 @@ import numpy as np
 import xarray as xr
 import pynanigans as pn
 from cycler import cycler
-from aux00_utils import check_simulation_completion
+from aux00_utils import check_simulation_completion, aggregate_parameters
 from aux01_physfuncs import get_topography_masks
 from colorama import Fore, Back, Style
 from dask.diagnostics import ProgressBar
@@ -15,12 +15,13 @@ print("Starting h00 script")
 #+++ Define run options
 simname_base = "tokara"
 
-resolutions    = cycler(res = [8, 4, 2, 1])
-resolutions    = cycler(res = [8, 4, 2])
 slopes         = cycler(α = [0.05, 0.1, 0.2])
 slopes         = cycler(α = [0.2,])
 Rossby_numbers = cycler(Ro_h = [0.08, 0.2, 0.5, 1.25])
 Froude_numbers = cycler(Fr_h = [0.08, 0.2, 0.5, 1.25])
+
+resolutions    = cycler(res = [8, 4, 2, 1])
+resolutions    = cycler(res = [8, 4, 2])
 closures       = cycler(closure = ["AMD"])
 closures       = cycler(closure = ["CSM"])
 bcs            = cycler(bounded = [0, 1,])
@@ -28,12 +29,13 @@ bcs            = cycler(bounded = [0, 1,])
 paramspace = slopes * Rossby_numbers * Froude_numbers
 configs    = resolutions * closures * bcs
 
-runs = resolutions * paramspace * closures * bcs
+runs = paramspace * configs
 #---
 
 for config in configs:
     print(config)
-    continue
-    run_options = aggregate_parameters(modifiers)
-    simname = f"{simname_base}_" + aggregate_parameters(modifiers, sep="_", prefix="")
+    config_suffix = aggregate_parameters(config, sep="_", prefix="")
+    simnames = [ "tokara_" + aggregate_parameters(params, sep="_", prefix="") + "_" + config_suffix for params in paramspace ]
+    check_simulation_completion(simnames, slice_name="tti", path="simulations/data/")
+
 pause
--- simulations/run_all_simulations_pbs.py
+++ simulations/run_all_simulations_pbs.py
@@ -7,16 +7,19 @@ from aux00_utils import aggregate_parameters
 #+++ Define run options
 simname_base = "tokara"
 
-resolutions    = cycler(res = [8, 4, 2, 1])
-resolutions    = cycler(res = [8, 4, 2])
 slopes         = cycler(α = [0.05, 0.1, 0.2])
 slopes         = cycler(α = [0.2,])
 Rossby_numbers = cycler(Ro_h = [0.08, 0.2, 0.5, 1.25])
 Froude_numbers = cycler(Fr_h = [0.08, 0.2, 0.5, 1.25])
-closures       = cycler(closure = ["AMD"])
-bcs            = cycler(bounded = [0,])
 
-runs = resolutions * slopes * Rossby_numbers * Froude_numbers * closures * bcs
+resolutions    = cycler(res = [8, 4, 2,])
+closures       = cycler(closure = ["AMD"])
+bcs            = cycler(bounded = [0])
+
+paramspace = slopes * Rossby_numbers * Froude_numbers
+configs    = resolutions * closures * bcs
+
+runs = paramspace * configs
 #---
 
 #+++ Options

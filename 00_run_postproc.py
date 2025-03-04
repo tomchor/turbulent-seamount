f4a2ae3a6251554eaef090bd885e3fbc66081848
tomchor <tomaschor@gmail.com>
2025-03-03 19:50:23 -0700

    go back to Tokara parameters

--- 00_run_postproc.py
+++ 00_run_postproc.py
@@ -15,16 +15,14 @@ print("Starting h00 script")
 #+++ Define run options
 simname_base = "tokara"
 
-slopes         = cycler(α = [0.05, 0.1, 0.2])
-slopes         = cycler(α = [0.2,])
-Rossby_numbers = cycler(Ro_h = [0.08, 0.2, 0.5, 1.25])
-Froude_numbers = cycler(Fr_h = [0.08, 0.2, 0.5, 1.25])
+slopes         = cycler(α = [0.05])
+Rossby_numbers = cycler(Ro_h = [1.4])
+Froude_numbers = cycler(Fr_h = [0.6])
 
 resolutions    = cycler(res = [8, 4, 2, 1])
 resolutions    = cycler(res = [8, 4, 2])
 closures       = cycler(closure = ["AMD"])
-closures       = cycler(closure = ["CSM"])
-bcs            = cycler(bounded = [0, 1,])
+bcs            = cycler(bounded = [0])
 
 paramspace = slopes * Rossby_numbers * Froude_numbers
 configs    = resolutions * closures * bcs
@@ -38,4 +36,3 @@ for config in configs:
     simnames = [ "tokara_" + aggregate_parameters(params, sep="_", prefix="") + "_" + config_suffix for params in paramspace ]
     check_simulation_completion(simnames, slice_name="tti", path="simulations/data/")
 
-pause
--- simulations/run_all_simulations_pbs.py
+++ simulations/run_all_simulations_pbs.py
@@ -7,13 +7,12 @@ from aux00_utils import aggregate_parameters
 #+++ Define run options
 simname_base = "tokara"
 
-slopes         = cycler(α = [0.05, 0.1, 0.2])
-slopes         = cycler(α = [0.2,])
-Rossby_numbers = cycler(Ro_h = [0.08, 0.2, 0.5, 1.25])
-Froude_numbers = cycler(Fr_h = [0.08, 0.2, 0.5, 1.25])
+slopes         = cycler(α = [0.05])
+Rossby_numbers = cycler(Ro_h = [1.4])
+Froude_numbers = cycler(Fr_h = [0.6])
 
 resolutions    = cycler(res = [8, 4, 2,])
-closures       = cycler(closure = ["AMD"])
+closures       = cycler(closure = ["AMD", "CSM"])
 bcs            = cycler(bounded = [0])
 
 paramspace = slopes * Rossby_numbers * Froude_numbers

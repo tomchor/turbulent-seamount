8775e1c7f97509f67a5101d63c8aa312c4638638
tomchor <tomaschor@gmail.com>
2025-05-16 09:15:46 -0600

    modify inv04 and inv08

--- inv04_mixing_efficiency.py
+++ inv04_mixing_efficiency.py
@@ -13,15 +13,14 @@ from aux02_plotting import letterize, create_mc, mscatter
 path = "simulations/data/"
 simname_base = "seamount"
 
-slopes         = cycler(α = [0.05, 0.2])
 Rossby_numbers = cycler(Ro_h = [0.2, 1.25])
 Froude_numbers = cycler(Fr_h = [0.2, 1.25])
+L              = cycler(L = [0, 300])
 
-resolutions    = cycler(dz = [8, 4, 2,])
-closures       = cycler(closure = ["AMD", "CSM", "DSM", "NON"])
-closures       = cycler(closure = ["AMD", "CSM", "DSM"])
+resolutions    = cycler(dz = [4, 2,])
+closures       = cycler(closure = ["AMD", "CSM",])
 
-paramspace = slopes * Rossby_numbers * Froude_numbers
+paramspace = Rossby_numbers * Froude_numbers * L
 configs    = resolutions * closures
 
 runs = paramspace * configs
@@ -34,49 +33,46 @@ bulk = bulk.reindex(Ro_h = list(reversed(bulk.Ro_h)))
 
 #+++ Define new variables
 bulk["γ⁵"] = bulk["∭⁵ε̄ₚdV"] / (bulk["∭⁵ε̄ₚdV"] + bulk["∭⁵ε̄ₖdV"])
-bulk["γ¹⁰"] = bulk["∭¹⁰ε̄ₚdV"] / (bulk["∭¹⁰ε̄ₚdV"] + bulk["∭¹⁰ε̄ₖdV"])
 
-bulk["H"]  = bulk.α * bulk.L
 bulk["RoFr"] = bulk.Ro_h * bulk.Fr_h
 
 bulk["𝒦ℰ"] = bulk["⟨∬⁵Ek′dxdy⟩ₜ"]
 bulk["𝒫"] = bulk["⟨∬⁵Πdxdy⟩ₜ"]
 bulk["ℬ"] = bulk["⟨∬⁵w′b′dxdy⟩ₜ"]
 
-bulk["ℰₖ"] = bulk["∭⁵ε̄ₖdV"] / (bulk.attrs["V∞"]**3 * bulk.L * bulk.H)
-bulk["ℰₚ"] = bulk["∭⁵ε̄ₚdV"] / (bulk.attrs["V∞"]**3 * bulk.L * bulk.H)
+bulk["ℰₖ"] = bulk["∭⁵ε̄ₖdV"] / (bulk.attrs["V∞"]**3 * bulk.FWMH * bulk.H)
+bulk["ℰₚ"] = bulk["∭⁵ε̄ₚdV"] / (bulk.attrs["V∞"]**3 * bulk.FWMH * bulk.H)
 
-bulk["𝒦⁵"] = (bulk["∭⁵ε̄ₚdV"] / bulk["N²∞"]) / (bulk["V∞"] * bulk.L**2 * bulk.H**2)
+bulk["𝒦⁵"] = (bulk["∭⁵ε̄ₚdV"] / bulk["N²∞"]) / (bulk["V∞"] * bulk.FWMH**2 * bulk.H**2)
 #---
 
 #+++ Make it legible
 bulk["𝒦ℰ"].attrs = dict(long_name=r"Norm TKE $\mathcal{KE}$")
 bulk["𝒦⁵"].attrs = dict(long_name=r"Norm buoyancy diffusivity $\mathcal{K}$")
-#bulk["𝒦¹⁰"].attrs = dict(long_name=r"Norm buoyancy diffusivity $\mathcal{K}$")
 bulk["𝒫"].attrs = dict(long_name=r"Norm shear prod rate $\mathcal{P}$")
 #---
 
 figs = []
 
-#bulk.plot.scatter(x="Slope_Bu", y="γ⁵", hue="α", col="dz", row="closure", xscale="log", yscale="log", cmap="bwr")
+#bulk.plot.scatter(x="Slope_Bu", y="γ⁵", hue="L", col="dz", row="closure", xscale="log", yscale="log", cmap="bwr")
 #figs.append(plt.gcf())
 
-bulk.plot.scatter(x="Slope_Bu", y="ℬ", hue="α", col="dz", row="closure", xscale="log", yscale="symlog", cmap="bwr")
-for ax in plt.gcf().axes:
+bulk.plot.scatter(x="Slope_Bu", y="ℬ", hue="L", col="dz", row="closure", xscale="log", yscale="symlog", cmap="bwr")
+for ax in plt.gcf().axes[:-1]:
     ax.set_yscale('symlog', linthresh=1e-3)
 figs.append(plt.gcf())
 
 
-#bulk.plot.scatter(x="RoFr", y="𝒦⁵", hue="α", col="dz", row="closure", xscale="log", yscale="log", cmap="bwr")
+#bulk.plot.scatter(x="RoFr", y="𝒦⁵", hue="L", col="dz", row="closure", xscale="log", yscale="log", cmap="bwr")
 #figs.append(plt.gcf())
 
-bulk.plot.scatter(x="Slope_Bu", y="𝒦ℰ", hue="α", col="dz", row="closure", xscale="log", yscale="log", cmap="bwr")
-figs.append(plt.gcf())
+#bulk.plot.scatter(x="Slope_Bu", y="𝒦ℰ", hue="L", col="dz", row="closure", xscale="log", yscale="log", cmap="bwr")
+#figs.append(plt.gcf())
 
-bulk.plot.scatter(x="Slope_Bu", y="𝒫", hue="α", col="dz", row="closure", xscale="log", yscale="log", cmap="bwr")
-figs.append(plt.gcf())
+#bulk.plot.scatter(x="Slope_Bu", y="𝒫", hue="L", col="dz", row="closure", xscale="log", yscale="log", cmap="bwr")
+#figs.append(plt.gcf())
 
-#bulk.plot.scatter(x="Slope_Bu", y="ℰₖ", hue="α", col="dz", row="closure", xscale="log", yscale="log", cmap="bwr")
+#bulk.plot.scatter(x="Slope_Bu", y="ℰₖ", hue="L", col="dz", row="closure", xscale="log", yscale="log", cmap="bwr")
 #figs.append(plt.gcf())
 
 for fig in figs:

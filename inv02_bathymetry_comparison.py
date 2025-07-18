f113a769191bcff6e07885429064673acbb1fbc4
tomchor <tomaschor@gmail.com>
2025-07-17 20:14:01 -0600

    add azimuthally-averaged spectrum plots in inv02

--- inv02_bathymetry_comparison.py
+++ inv02_bathymetry_comparison.py
@@ -33,19 +33,44 @@ aaai = merge_datasets(runs, base_name=f"aaai.{simname_base}", dirpath=path, verb
 aaai = aaai[["bottom_height"]]
 #---
 
+#+++ Create Gaussian seamount with height H and width FWHM and merge with bottom height
+r = np.sqrt(aaai.x_caa**2 + aaai.y_aca**2)
+h_gaussian = aaai.H * np.exp(-np.log(2) * (r/aaai.FWHM)**2)
+
+# Merge the datasets along L dimension
+L_gaussian = 2 * aaai.L.values[-1]
+aaai = aaai.reindex(L=list(aaai.L.values) + [L_gaussian])
+aaai["bottom_height"].loc[dict(L=L_gaussian)] = h_gaussian.T
+#---
+
+#+++ Compute FFT and 2D power spectrumof bottom height
 aaai["h_fft"] = xrft.fft(aaai.bottom_height, true_phase=True, true_amplitude=True, dim=("x_caa", "y_aca"))
 aaai["h_fft_amp"] = np.abs(aaai.h_fft)
 
-aaai["S_h"] = (aaai.h_fft * np.conjugate(aaai.h_fft)).real
+aaai["S_h_2d"] = (aaai.h_fft * np.conjugate(aaai.h_fft)).real
+#---
 
-Sh_pos = aaai.S_h.where(aaai.freq_y_aca>0, drop=True).sel(freq_x_caa=0, freq_y_aca=slice(0, None))
-k_cm = (Sh_pos * Sh_pos.freq_y_aca).sum("freq_y_aca") / Sh_pos.sum("freq_y_aca")
+#+++ Compute isotropic power spectrum of bottom height and Gaussian seamount
+aaai["S_h"] = xrft.isotropic_power_spectrum(aaai.bottom_height,
+                                            dim = ["x_caa", "y_aca"],
+                                            nfactor = 4, # Number of radial bins relative to data size
+                                            truncate = True, # Truncate at Nyquist frequency
+                                            true_phase = True, # Preserve phase information
+                                            true_amplitude = True # Proper amplitude scaling
+                                            )
+
+#---
+
+#+++ Compute characteristic length scale of bottom height and Gaussian seamount
+k_cm = (aaai.S_h * aaai.S_h.freq_r).sum("freq_r") / aaai.S_h.sum("freq_r")
 aaai["L_cm"] = 1/k_cm
+#---
 
-# Create Gaussian seamount with height H and width FWHM
-x = aaai.x_caa
-y = aaai.y_aca
-X, Y = np.meshgrid(x, y, indexing='ij')
-R = np.sqrt(X**2 + Y**2)
+#+++ Plot results
+fig, axes = plt.subplots(ncols=2, figsize=(12, 5))
 
-aaai["h_gaussian"] = aaai.H * np.exp(-4*np.log(2) * (R/aaai.FWHM)**2)
+aaai.bottom_height.sel(x_caa=0, method="nearest").plot(hue="L", ax=axes[0])
+
+aaai.S_h.plot(hue="L", ax=axes[1], xscale="log", yscale="log")
+axes[1].axvline(1/aaai.FWHM, color="black", linestyle="--", label="FWHM")
+axes[1].axvline(2*np.pi/aaai.FWHM, color="black", linestyle="--", label="FWHM/2$\pi$")

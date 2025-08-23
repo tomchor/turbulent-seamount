a0dc274fc6cecb4a9908c9a8ded91959f9a13ec5
Tomás Chor <tomaschor@gmail.com>
2025-08-22 18:54:00 -0700

    Create a 3D figure with a routh and smooth seamount (#47)

--- simulations/plot_3d_animation.jl
+++ simulations/plot_3d_animation.jl
@@ -4,8 +4,8 @@ using GLMakie
 using Printf
 using Oceananigans: prettytime
 
-# Data file path
-fpath_xyzi = (@isdefined simulation) ? simulation.output_writers[:nc_xyzi].filepath : "data/xyzi.seamount_Ro_h=0.2_Fr_h=1.25_L=0_dz=4_closure=DSM.nc"
+#+++ Preamble
+fpath_xyzi = (@isdefined simulation) ? simulation.output_writers[:nc_xyzi].filepath : "data/xyzi.seamount_Ro_h0.2_Fr_h1.25_L0.8_dz2.nc"
 
 @info "Reading NetCDF file: $fpath_xyzi"
 xyzi = RasterStack(fpath_xyzi, name=(:PV, :εₖ, :εₚ, :bottom_height), lazy=true)
@@ -20,8 +20,9 @@ x_range = extrema(dims(xyzi, :x_caa))
 y_range = extrema(dims(xyzi, :y_aca))
 z_range = extrema(dims(xyzi, :z_aac))
 times = dims(xyzi.PV, :Ti)
+#---
 
-#region Set limits based on data range or physical considerations
+#+++ Set limits based on data range or physical considerations
 interior_q = params.N²∞ * params.f₀
 isovalue_q = 1.4 * interior_q  # Adjust this based on data
 isorange_q = isovalue_q/10
@@ -38,7 +39,7 @@ isovalue_εₚ₂ = 1e-9
 εₚ_range = (isovalue_εₚ₁, isovalue_εₚ₂)
 isorange_εₚ₁ = isovalue_εₚ₁/2
 isorange_εₚ₂ = isovalue_εₚ₂/2
-#endregion
+#---
 
 # 3D plot settings
 Lx = diff(x_range |> collect)[]
@@ -62,29 +63,29 @@ ax1 = Axis3(fig[1, 1]; settings_axis3...)
 ax2 = Axis3(fig[1, 2]; settings_axis3...)
 ax3 = Axis3(fig[1, 3]; settings_axis3...)
 
-#region bottom height plot
+#+++ bottom height plot
 for ax in (ax1, ax2, ax3)
     surface!(ax, x_range, y_range, xyzi.bottom_height, colormap = :turbid)
 end
-#endregion
+#---
 
-#region PV plot
+#+++ PV plot
 vol1_kwargs = (algorithm = :iso, colormap=:balance, transparency = true, isorange=isorange_q, colorrange=PV_range)
 vol1 = volume!(ax1, x_range, y_range, z_range, PVₙ, isovalue=-isovalue_q; alpha=0.9, vol1_kwargs...)
 vol1 = volume!(ax1, x_range, y_range, z_range, PVₙ, isovalue=+isovalue_q; alpha=0.7, vol1_kwargs...)
 Colorbar(fig, vol1, bbox=ax1.scene.viewport,
          label="PV", height=15, width=Relative(0.5), vertical=false,
          alignmode = Outside(10), halign = 0.15, valign = 1.02)
-#endregion
+#---
 
-#region εₖ plot
+#+++ εₖ plot
 vol2_kwargs = (algorithm = :iso, colormap=Reverse(:roma), transparency = true, colorrange=εₖ_range)
 vol2 = volume!(ax2, x_range, y_range, z_range, εₖₙ, isovalue=isovalue_εₖ₁, isorange=isorange_εₖ₁, alpha=0.7; vol2_kwargs...)
 vol2 = volume!(ax2, x_range, y_range, z_range, εₖₙ, isovalue=isovalue_εₖ₂, isorange=isorange_εₖ₂; alpha=0.9, vol2_kwargs...)
 Colorbar(fig, vol2, bbox=ax2.scene.viewport,
          label="εₖ", height=15, width=Relative(0.5), vertical=false,
          alignmode = Outside(10), halign = 0.5, valign = 1.02)
-#endregion
+#---
 
 #+++ εₚ plot
 vol3_kwargs = (algorithm = :iso, colormap=Reverse(:roma), transparency = true, colorrange=εₚ_range)

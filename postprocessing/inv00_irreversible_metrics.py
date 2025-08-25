dcc438ddb5482770db3bed63c103c134f9cfae36
tomchor <tomaschor@gmail.com>
2025-08-25 13:20:58 -0600

    increase horizontal length

--- postprocessing/inv00_irreversible_metrics.py
+++ postprocessing/inv00_irreversible_metrics.py
@@ -17,9 +17,9 @@ simname_base = "seamount"
 Rossby_numbers = cycler(Ro_h = [0.2])
 Froude_numbers = cycler(Fr_h = [1.25])
 L              = cycler(L = [0, 0.05, 0.1, 0.2, 0.4, 0.8,
-                             0.8, 0.8, 0.8, 0.8])
-FWHM           = cycler(FWHM = [300, 300, 300, 300, 300, 300,
-                                30, 60, 90, 120])
+                             0.8, 0.8])
+FWHM           = cycler(FWHM = [400, 400, 400, 400, 400, 400,
+                                100, 40])
 
 resolutions    = cycler(dz = [8, 4, 2])
 

b1d011677ba5817ccc8c97b710755fdf7d824d3c
tomchor <tomaschor@gmail.com>
2025-09-15 09:46:00 -0600

    rename figures

---
+++ postprocessing/plot01_integrated_dissipation.py
@@ -0,0 +1,41 @@
+import numpy as np
+import xarray as xr
+from matplotlib import pyplot as plt
+from matplotlib.colors import LogNorm
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+plt.rcParams["figure.constrained_layout.use"] = True
+
+seamounts = xr.open_dataset("../bathymetry/seamount_data.nc")
+seamounts["Slope_Bu"] = seamounts.rossby_number / seamounts.froude_height
+seamounts["dissip_height"] = seamounts.velocity**3 * seamounts.Slope_Bu / seamounts.height
+
+seamounts["total_volume"] = seamounts.basal_radius_L**2 * seamounts.height
+seamounts["total_dissip_height"] = seamounts.dissip_height * seamounts.total_volume
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 5), sharey=True,
+                         subplot_kw={"projection": ccrs.PlateCarree()},
+                         gridspec_kw={"width_ratios": [3, 1]})
+
+#+++ Plot scatter plot on the left axis
+fixed_options = dict(x="longitude", y="latitude", edgecolors="face", s=1, transform=ccrs.PlateCarree())
+
+# Add land features to all subplots
+ax = axes[0]
+ax.add_feature(cfeature.LAND, color="black", zorder=0)
+ax.add_feature(cfeature.COASTLINE, color="gray", linewidth=0.5, zorder=1)
+ax.set_global()
+
+seamounts.plot.scatter(ax=ax, hue="total_dissip_height", **fixed_options, norm=LogNorm(), vmin=1e3, vmax=1e6)
+#---
+
+#+++ Plot integrated line plot on the right axis
+# Bin seamounts data by latitude and longitude with 1 degree resolution
+lat_bins = np.arange(-90, 91, 1)
+
+# Create binned statistics for various quantities
+print("Binning data")
+binned_seamounts = seamounts.groupby_bins("latitude", lat_bins).sum()
+binned_seamounts.total_dissip_height.plot(ax=axes[1], y="latitude_bins")
+#---

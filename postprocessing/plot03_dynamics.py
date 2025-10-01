1156318598d2fc40a7bb938d265e3f62dc365ac7
tomchor <tomaschor@gmail.com>
2025-10-01 08:03:48 -0600

    include w'b' plot

--- postprocessing/plot03_dynamics.py
+++ postprocessing/plot03_dynamics.py
@@ -26,11 +26,13 @@ opensim_opts = dict(unique_times=False,
                     open_dataset_kwargs=dict(chunks="auto"))
 xyzi_L00 = open_simulation(simdata_path + f"xyzi.seamount_Ro_h0.1_Fr_h1_L0_FWHM500_{resolution}.nc", **snap_opts)
 xyzd_L00 = open_simulation(postproc_path + f"xyzd.seamount_Ro_h0.1_Fr_h1_L0_FWHM500_{resolution}.nc", **opensim_opts)
-ds_L00 = xr.merge([xyzi_L00, xyzd_L00])
+turb_L00 = open_simulation(postproc_path + f"turbstats_seamount_Ro_h0.1_Fr_h1_L0_FWHM500_{resolution}.nc", **opensim_opts)
+ds_L00 = xr.merge([xyzi_L00, xyzd_L00, turb_L00])
 
 xyzi_L08 = open_simulation(simdata_path + f"xyzi.seamount_Ro_h0.1_Fr_h1_L0.8_FWHM500_{resolution}.nc", **snap_opts)
 xyzd_L08 = open_simulation(postproc_path + f"xyzd.seamount_Ro_h0.1_Fr_h1_L0.8_FWHM500_{resolution}.nc", **opensim_opts)
-ds_L08 = xr.merge([xyzi_L08, xyzd_L08])
+turb_L08 = open_simulation(postproc_path + f"turbstats_seamount_Ro_h0.1_Fr_h1_L0.8_FWHM500_{resolution}.nc", **opensim_opts)
+ds_L08 = xr.merge([xyzi_L08, xyzd_L08, turb_L08])
 #---
 
 #+++ Create new variables and restrict volume
@@ -47,9 +49,11 @@ def prepare_ds(ds,
     mask = ds.distance_condition_5meters
     masked_volume = (mask * ds.Δz_aac).pnsum("z").load()
 
+    ds["PV_zavg"] = (mask * ds["PV"] * ds.Δz_aac).pnsum("z") / masked_volume
     ds["Ro_zavg"] = (mask * ds["Ro"] * ds.Δz_aac).pnsum("z") / masked_volume
     ds["εₖ_zavg"] = (mask * ds["εₖ"] * ds.Δz_aac).pnsum("z") / masked_volume
     ds["εₚ_zavg"] = (mask * ds["εₚ"] * ds.Δz_aac).pnsum("z") / masked_volume
+    ds["wb_zavg"] = (mask * ds["⟨w′b′⟩ₜ"] * ds.Δz_aac).pnsum("z") / masked_volume
     
     return ds
 
@@ -60,9 +64,9 @@ ds_L08 = prepare_ds(ds_L08)
 print("Data preparation complete!")
 #---
 
-#+++ Create 4x2 subplot grid
+#+++ Create 5x2 subplot grid
 print("Creating subplot grid")
-fig, axes = plt.subplots(nrows=4, ncols=2, figsize=(15, 15), sharex=True, layout=None)
+fig, axes = plt.subplots(nrows=5, ncols=2, figsize=(15, 15), sharex=True, layout=None)
 plt.subplots_adjust(wspace=0.05, hspace=0)
 #---
 
@@ -79,12 +83,12 @@ for i, (ds, L_str) in enumerate(datasets):
     ax = axes[0, i]
     # Data is already loaded and time-selected
     pv_data = ds.PV.pnsel(**xyi_sel_opts)
-    im = pv_data.pnimshow(ax=ax, x="x", y="y",
-                        cmap="RdBu_r",
-                        add_colorbar=False,
-                        rasterized=True,
-                        vmin = -1.5*PV_inf,
-                        vmax = +1.5*PV_inf)
+    im = pv_data.plot.imshow(ax=ax, x="x_caa",
+                             cmap="RdBu_r",
+                             add_colorbar=False,
+                             rasterized=True,
+                             vmin = -1.5*PV_inf,
+                             vmax = +1.5*PV_inf)
     ax.set_title(f"L/FWHM = {L_str}")
     ax.set_xlabel("")
     ax.set_yticks(yticks)
@@ -106,12 +110,12 @@ print("Plotting Ro_zavg")
 for i, (ds, L_str) in enumerate(datasets):
     ax = axes[1, i]
     # Data is already loaded and processed
-    im = ds.Ro_zavg.pnimshow(ax=ax, x="x",
-                             cmap="RdBu_r",
-                             add_colorbar=False,
-                             rasterized=True,
-                             vmin = -0.4,
-                             vmax = +0.4)
+    im = ds.Ro_zavg.plot.imshow(ax=ax, x="x_caa",
+                                cmap="RdBu_r",
+                                add_colorbar=False,
+                                rasterized=True,
+                                vmin = -0.4,
+                                vmax = +0.4)
     ax.set_title("")
     ax.set_xlabel("")
     ax.set_yticks(yticks)
@@ -132,11 +136,11 @@ print("Plotting εₖ_zavg")
 for i, (ds, L_str) in enumerate(datasets):
     ax = axes[2, i]
     # Data is already loaded and processed
-    im = ds["εₖ_zavg"].pnimshow(ax=ax, x="x", y="y",
-                              cmap="inferno",
-                              add_colorbar=False,
-                              rasterized=True,
-                              norm=LogNorm(vmin=1e-10, vmax=1e-8))
+    im = ds["εₖ_zavg"].plot.imshow(ax=ax, x="x_caa",
+                                   cmap="inferno",
+                                   add_colorbar=False,
+                                   rasterized=True,
+                                   norm=LogNorm(vmin=1e-10, vmax=1e-8))
     ax.set_title("")
     ax.set_xlabel("")
     ax.set_yticks(yticks)
@@ -157,11 +161,11 @@ print("Plotting εₚ_zavg")
 for i, (ds, L_str) in enumerate(datasets):
     ax = axes[3, i]
     # Data is already loaded and processed
-    im = ds["εₚ_zavg"].pnimshow(ax=ax, x="x", y="y",
-                              cmap="inferno",
-                              add_colorbar=False,
-                              rasterized=True,
-                              norm=LogNorm(vmin=1e-11, vmax=1e-9))
+    im = ds["εₚ_zavg"].plot.imshow(ax=ax, x="x_caa",
+                                   cmap="inferno",
+                                   add_colorbar=False,
+                                   rasterized=True,
+                                   norm=LogNorm(vmin=1e-11, vmax=1e-9))
     ax.set_title("")
     ax.set_xlabel("x [m]")
     ax.set_yticks(yticks)
@@ -175,6 +179,32 @@ for i, (ds, L_str) in enumerate(datasets):
 divider = make_axes_locatable(axes[3, 1])
 cbar_ax = divider.append_axes("right", size="5%", pad=0.1)
 plt.colorbar(im, cax=cbar_ax, label="εₚ", shrink=0.5)
+#---
+
+#+++ Plot wb_zavg
+print("Plotting wb_zavg")
+for i, (ds, L_str) in enumerate(datasets):
+    ax = axes[4, i]
+    # Data is already loaded and processed
+    im = ds["wb_zavg"].plot.imshow(ax=ax, x="x_caa",
+                                   cmap="RdBu_r",
+                                   add_colorbar=False,
+                                   rasterized=True,
+                                   vmin=-4e-11,
+                                   vmax=+4e-11)
+    ax.set_title("")
+    ax.set_xlabel("x [m]")
+    ax.set_yticks(yticks)
+    ax.set_aspect('equal')
+    if i == 0:
+        ax.set_ylabel("y [m]")
+    else:
+        ax.set_ylabel("")
+
+# Add colorbar for wb_zavg row (aligned with rightmost panel)
+divider = make_axes_locatable(axes[4, 1])
+cbar_ax = divider.append_axes("right", size="5%", pad=0.1)
+plt.colorbar(im, cax=cbar_ax, label="wb", shrink=0.5)
 #---
 
 #+++ Save

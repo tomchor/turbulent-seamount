2d7fcff7ebce3ff16b8aa02658c5613f60678833
tomchor <tomaschor@gmail.com>
2025-10-07 16:47:25 -0600

    max out RAM for largemem

--- postprocessing/run_postproc_pbs.sh
+++ postprocessing/run_postproc_pbs.sh
@@ -5,7 +5,7 @@
 #PBS -e logs/postproc.log
 #PBS -l walltime=24:00:00
 #PBS -q largemem
-#PBS -l select=1:ncpus=18:mem=1000GB:ngpus=0
+#PBS -l select=1:ncpus=18:mem=1500GB:ngpus=0
 #PBS -M tchor@umd.edu
 #PBS -m abe
 #PBS -r n

21f29e1839d24f3b93f910221a444bfefae74283
tomchor <tomaschor@gmail.com>
2025-10-17 06:30:45 -0600

    alignment and cleanuo

--- paramsweep_postprocessing/00_run_postproc.py
+++ paramsweep_postprocessing/00_run_postproc.py
@@ -1,15 +1,10 @@
 import sys
 sys.path.append("/glade/u/home/tomasc/repos/pynanigans")
 sys.path.append("../postprocessing")
-import subprocess
 import numpy as np
-import xarray as xr
-import pynanigans as pn
 from cycler import cycler
 from src.aux00_utils import check_simulation_completion, aggregate_parameters
-from src.aux01_physfuncs import get_topography_masks
 from colorama import Fore, Back, Style
-from dask.diagnostics import ProgressBar
 
 print("Starting h00 script")
 
@@ -17,11 +12,11 @@ print("Starting h00 script")
 path = "../simulations/data/"
 simname_base = "seamount"
 
-Rossby_numbers = cycler(Ro_h = [0.05, 0.1, 0.2, 0.5])
-Froude_numbers = cycler(Fr_h = [0.02, 0.08, 0.3, 1])
-L              = cycler(L = [0, 0.8])
+Rossby_numbers      = cycler(Ro_h = [0.05, 0.1, 0.2, 0.5])
+Froude_numbers      = cycler(Fr_h = [0.05, 0.08, 0.3, 1, 2])
+L                   = cycler(L = [0, 0.8])
 
-resolutions    = cycler(dz = [1])
+resolutions         = cycler(dz = [2])
 T_advective_spinups = cycler(T_advective_spinup = [12])
 
 paramspace = Rossby_numbers * Froude_numbers * L

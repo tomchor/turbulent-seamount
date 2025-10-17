4054943ee4204b8b0db3fc983a120fd769bafc87
tomchor <tomaschor@gmail.com>
2025-10-17 12:03:18 -0600

    fix merge errors

--- simulations/run_parameter_sweep.py
+++ simulations/run_parameter_sweep.py
@@ -8,11 +8,11 @@ Froude_numbers = cycler(Fr_b = [0.05, 0.08, 0.3, 1, 2])
 L              = cycler(L = [0, 0.8])
 
 # Define numerical parameters
-resolutions    = cycler(dz = [4, 2, 1])
-T_adv_spinups  = cycler(T_advective_spinup = [12])
+resolutions    = cycler(dz = [4, 2])
+T_adv_spinups  = cycler(T_adv_spinup = [12])
 
 paramspace = Rossby_numbers * Froude_numbers * L
-configs    = resolutions * T_advective_spinups
+configs    = resolutions * T_adv_spinups
 
 runs = paramspace * configs
 #---

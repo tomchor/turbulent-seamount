e67cacd7ada1db3b00ade0d6670a2afefaff642d
tomchor <tomaschor@gmail.com>
2025-10-31 08:22:16 -0600

    much better plot04

--- postprocessing/plot04_bulk_metrics.py
+++ postprocessing/plot04_bulk_metrics.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append("/glade/u/home/tomasc/repos/pynanigans")
 import pynanigans as pn
+import numpy as np
 import xarray as xr
 from cycler import cycler
 from matplotlib import pyplot as plt
@@ -29,7 +30,7 @@ aaaa = merge_datasets(runs, base_name=f"aaaa.{simname_base}", verbose=True, add_
 aaaa = aaaa.reindex(Ro_b = list(reversed(aaaa.Ro_b)))
 
 # Load aaad datasets to get additional variables
-aaad = merge_datasets(runs, base_name=f"aaad.{simname_base}", verbose=True, add_min_spacings=False, keep_vars=["∭⟨w′b′⟩ₜdV", "∭SPRdV"],
+aaad = merge_datasets(runs, base_name=f"aaad.{simname_base}", verbose=True, add_min_spacings=False, keep_vars=["∭⟨w′b′⟩ₜdV", "∭SPRdV", "U∞∬⟨Ek′⟩ₜdxdz"],
                       combine_by_coords_kwargs=dict(compat="override", combine_attrs="drop_conflicts", coords="minimal"))
 aaad = aaad.reindex(Ro_b = list(reversed(aaad.Ro_b)))
 
@@ -39,8 +40,7 @@ aaaa = xr.merge([aaaa, aaad], compat="override")
 #+++ Process data and create derived variables
 # Condense buffer variables
 for var in ["ε̄ₚ", "ε̄ₖ"]:
-    aaaa = condense(aaaa, [f"∭⁵{var}dV", f"∭¹⁰{var}dV"], f"∭ᵇ{var}dV",
-                   dimname="buffer", indices=[5, 10])
+    aaaa = condense(aaaa, [f"∭⁵{var}dV", f"∭¹⁰{var}dV"], f"∭ᵇ{var}dV", dimname="buffer", indices=[5, 10])
 
 # Create normalized variables
 aaaa["γ"] = aaaa["∭ᵇε̄ₚdV"] / (aaaa["∭ᵇε̄ₚdV"] + aaaa["∭ᵇε̄ₖdV"])
@@ -50,48 +50,45 @@ aaaa["RoFr"] = aaaa.Ro_b * aaaa.Fr_b
 dtKE_scaling = aaaa.attrs["U∞"]**3 * aaaa.FWHM**2 # Assume ε̄ₖ scales as U^3 / H
 aaaa["ℰₖ"] = aaaa["∭ᵇε̄ₖdV"] / dtKE_scaling
 aaaa["ℰₚ"] = aaaa["∭ᵇε̄ₚdV"] / dtKE_scaling
-aaaa["ℬ"] = aaaa["∭⟨w′b′⟩ₜdV"] / dtKE_scaling
+aaaa["ℬ"] = -aaaa["∭⟨w′b′⟩ₜdV"] / dtKE_scaling
 aaaa["𝒮"] = aaaa["∭SPRdV"] / dtKE_scaling
+aaaa["𝒯"] = aaaa["U∞∬⟨Ek′⟩ₜdxdz"] / dtKE_scaling
 aaaa["𝒦⁵"] = (aaaa["∭ᵇε̄ₚdV"] / aaaa["N²∞"]) / (aaaa["U∞"] * aaaa.FWHM**2 * aaaa.H**2)
 
 # Add metadata
-aaaa["𝒦⁵"].attrs = dict(long_name=r"Norm buoyancy diffusivity $\mathcal{K}$")
+aaaa["ℰₖ"].attrs = dict(long_name=r"TKE dissipation rate $\mathcal{E}_k$")
+aaaa["ℰₚ"].attrs = dict(long_name=r"Buoyancy dissipation rate $\mathcal{E}_p$")
+aaaa["ℬ"].attrs = dict(long_name=r"Turbulent buoyancy flux $\mathcal{B}$")
+aaaa["𝒮"].attrs = dict(long_name=r"Shear production rate $\mathcal{S}$")
+aaaa["𝒯"].attrs = dict(long_name=r"TKE advection out $\mathcal{T}$")
+aaaa["𝒦⁵"].attrs = dict(long_name=r"Buoyancy diffusivity $\mathcal{K}$")
 #---
 
-#+++ Helper function to create scatter plot
-def plot_variable(ax, data, var_name):
-    """Create a scatter plot for a given variable on the specified axis"""
-    for fwhm_val in data.FWHM.values:
-        subset = data.sel(FWHM=fwhm_val)
-        ax.scatter(subset.L, subset.values, label=f"FWHM={fwhm_val}", alpha=0.7)
+#+++ Create single plot with all variables
+fig, ax = plt.subplots(figsize=(7, 5))
 
-    # Use symlog scale for w"b" variable (can be positive or negative)
-    if (var_name == "∭⟨w′b′⟩ₜdV") or (var_name == "ℬ"):
-        ax.set_yscale("symlog", linthresh=1e-6)
-    else:
-        ax.set_yscale("log")
+# Variables to plot
+variables = ["ℰₖ", "ℰₚ", "ℬ", "𝒮", "𝒯"]
+colors = ["blue", "red", "green", "orange", "purple"]
 
-    ax.set_xlabel("L")
-    ax.set_ylabel(var_name)
-    ax.set_title(f"{var_name}")
-    ax.grid(True)
-    ax.legend()
+aaaa = aaaa.pnsel(x=np.inf, method="nearest") # To get the advection out term
+aaaa = aaaa.sel(dz=0, buffer=5, method="nearest").sum("j", keep_attrs=True)
 
-#+++ Create plots
-fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 10), sharey="row")
-plt.subplots_adjust(hspace=0.4, wspace=0.3)
+# Plot each variable using xarray plot method
+for var_name, color in zip(variables, colors):
+    variable_da = aaaa[var_name]
+    variable_da.plot.scatter(ax=ax, x="L", label=variable_da.attrs["long_name"], color=color, marker="o")
 
-# Variables to plot (first 3 use buffer=5m, last 2 don"t have buffer dimension)
-variables = ["ℰₖ", "ℰₚ", "ℬ", "𝒮"]
-
-aaaa = aaaa.sel(dz=0, buffer=5, method="nearest").sum("j")
-# Create plots for each variable
-for i, var_name in enumerate(variables):
-    data = aaaa[var_name]
-    plot_variable(axes.flat[i], data, var_name)
+# Set log scale and formatting
+ax.set_yscale("log")
+ax.set_xlabel("L/W", fontsize=12)
+ax.set_ylabel("Value / ($U_\infty^3 L^2$)", fontsize=12)
+ax.set_title(f"Turbulent metrics vs smoothing scale ($Ro_b$={aaaa.Ro_b.item()}, $Fr_b$={aaaa.Fr_b.item()})", fontsize=14)
+ax.grid(True, which="both", alpha=0.3)
+ax.legend(fontsize=11, loc="upper right", framealpha=0.4)
 
 #+++ Save figure
-figure_name = f"../figures/bulk_metrics_{simname_base}_dz{aaaa.dz.item()}m_buffer{aaaa.buffer.item()}m.png"
+figure_name = f"../figures/bulk_metrics_{simname_base}_dz{aaaa.dz.item()}m_buffer{aaaa.buffer.item()}m.pdf"
 plt.savefig(figure_name, dpi=300, bbox_inches="tight")
 print(f"Figure saved to: {figure_name}")
 #---

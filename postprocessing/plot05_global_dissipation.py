7c9099bae064e5f9d862786f031cdce80fd38b10
tomchor <tomaschor@gmail.com>
2025-11-10 10:28:06 -0700

    final touches on plot05

--- postprocessing/plot05_global_dissipation.py
+++ postprocessing/plot05_global_dissipation.py
@@ -4,12 +4,14 @@ from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+from src.aux02_plotting import letterize
 
 plt.rcParams["figure.constrained_layout.use"] = True
 
 seamounts = xr.open_dataset("../bathymetry/seamount_data.nc")
 seamounts["Slope_Bu"] = seamounts.rossby_number / seamounts.froude_height
 ρ = 1025
+top_lat = 75
 
 #+++ Dissipation calculations
 # Dissipation according to linear formula (smooth seamounts)
@@ -47,7 +49,7 @@ seamounts.total_mixing_quadratic.attrs = dict(units="W", longer_name="Buoyancy m
 
 #+++ Create figure with projections
 fig = plt.figure(figsize=(10, 7))
-gs = fig.add_gridspec(2, 2, width_ratios=[3, 1])
+gs = fig.add_gridspec(2, 2, width_ratios=[3, 1], hspace=0)
 
 # Create left subplots with projection for heatmaps (2 rows)
 ax_map_dissip_smooth = fig.add_subplot(gs[0, 0], projection=ccrs.PlateCarree())
@@ -61,11 +63,6 @@ ax_plot_dissip = fig.add_subplot(gs[0:2, 1])
 fixed_options = dict(x="longitude", y="latitude", edgecolors="face", s=1, transform=ccrs.PlateCarree(), rasterized=True)
 
 # Row 1: KE dissipation for smooth seamounts
-ax_map_dissip_smooth.add_feature(cfeature.LAND, color="black", zorder=0)
-ax_map_dissip_smooth.add_feature(cfeature.COASTLINE, color="gray", linewidth=0.5, zorder=1)
-ax_map_dissip_smooth.set_ylim(-80, 80)
-ax_map_dissip_smooth.set_title("KE dissipation (smooth)")
-
 gl1 = ax_map_dissip_smooth.gridlines(draw_labels=True, dms=False, x_inline=False, y_inline=False)
 gl1.top_labels = False
 gl1.right_labels = False
@@ -76,26 +73,29 @@ vmax_dissip = 1e8
 cmap_dissip = "YlOrRd"
 im_dissip_smooth = seamounts.plot.scatter(ax=ax_map_dissip_smooth, hue="total_dissip_smooth", cmap=cmap_dissip, **fixed_options, norm=LogNorm(), vmin=vmin_dissip, vmax=vmax_dissip, add_colorbar=False)
 cbar_dissip_smooth = plt.colorbar(im_dissip_smooth, ax=ax_map_dissip_smooth, orientation="vertical", pad=0.02, location="left", shrink=0.5)
-cbar_dissip_smooth.set_label(r"KE dissipation [W]")
+cbar_dissip_smooth.set_label(r"Dissipation [W]")
+ax_map_dissip_smooth.add_feature(cfeature.LAND, color="black", zorder=0)
+ax_map_dissip_smooth.add_feature(cfeature.COASTLINE, color="gray", linewidth=0.5, zorder=1)
+ax_map_dissip_smooth.set_ylim(-top_lat, top_lat)
+ax_map_dissip_smooth.set_title("KE dissipation (smooth)")
 
 # Row 2: KE dissipation for rough seamounts
-ax_map_dissip_rough.add_feature(cfeature.LAND, color="black", zorder=0)
-ax_map_dissip_rough.add_feature(cfeature.COASTLINE, color="gray", linewidth=0.5, zorder=1)
-ax_map_dissip_rough.set_ylim(-80, 80)
-ax_map_dissip_rough.set_title("KE dissipation (rough)")
-
 gl2 = ax_map_dissip_rough.gridlines(draw_labels=True, dms=False, x_inline=False, y_inline=False)
 gl2.top_labels = False
 gl2.right_labels = False
 
 im_dissip_rough = seamounts.plot.scatter(ax=ax_map_dissip_rough, hue="total_dissip_rough", cmap=cmap_dissip, **fixed_options, norm=LogNorm(), vmin=vmin_dissip, vmax=vmax_dissip, add_colorbar=False)
 cbar_dissip_rough = plt.colorbar(im_dissip_rough, ax=ax_map_dissip_rough, orientation="vertical", pad=0.02, location="left", shrink=0.5)
-cbar_dissip_rough.set_label(r"KE dissipation [W]")
+cbar_dissip_rough.set_label(r"Dissipation [W]")
+ax_map_dissip_rough.add_feature(cfeature.LAND, color="black", zorder=0)
+ax_map_dissip_rough.add_feature(cfeature.COASTLINE, color="gray", linewidth=0.5, zorder=1)
+ax_map_dissip_rough.set_ylim(-top_lat, top_lat)
+ax_map_dissip_rough.set_title("KE dissipation (rough)")
 #---
 
 #+++ Plot integrated line plot on the right column (spanning 2 rows)
 # Bin seamounts data by latitude and longitude with 1 degree resolution
-lat_bins = np.arange(-80, 81, 1)
+lat_bins = np.arange(-top_lat, top_lat + 1, 1)
 
 # Create binned statistics for various quantities
 print("Binning data")
@@ -106,9 +106,10 @@ binned_seamounts.total_dissip_smooth.plot(ax=ax_plot_dissip, y="latitude_bins", 
 binned_seamounts.total_dissip_rough.plot(ax=ax_plot_dissip, y="latitude_bins", color="red", linestyle="--", label="Rough")
 ax_plot_dissip.set_ylabel("Latitude (degrees)")
 ax_plot_dissip.set_xlabel("KE dissipation [W]")
+ax_plot_dissip.set_ylim(-top_lat, top_lat)
 ax_plot_dissip.set_title("KE dissipation\nper degree of latitude")
 ax_plot_dissip.grid(True)
-ax_plot_dissip.legend()
+ax_plot_dissip.legend(loc="upper right", borderaxespad=0, framealpha=0.9, edgecolor="black", fancybox=False)
 #---
 
 #+++ Calculate difference
@@ -127,5 +128,6 @@ print(f"South mixing ratio: {south_mixing_ratio:.2f}")
 #---
 
 #+++ Save figure
+letterize(fig.axes[:3], x=0.05, y=0.92, fontsize=12, bbox=dict(boxstyle="square", facecolor="white", alpha=0.9))
 fig.savefig("../figures/global_dissipation.pdf", dpi=300, bbox_inches="tight", pad_inches=0)
 #---

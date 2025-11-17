02e4b07120be9ca6af039b847f1da38a221c068e
tomchor <tomaschor@gmail.com>
2025-11-17 10:23:39 -0700

    finished plot03 for now

--- postprocessing/plot03_eps_comparison.py
+++ postprocessing/plot03_eps_comparison.py
@@ -3,11 +3,11 @@ import xarray as xr
 from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
 from matplotlib.gridspec import GridSpec
-from src.aux00_utils import open_simulation
+from src.aux00_utils import open_simulation, condense
 from src.aux02_plotting import letterize
 
 #+++ Parameters
-simname_base = "labanus"
+simname_base = "balanus"
 simdata_path = "../simulations/data/"
 postproc_path = "../postprocessing/data/"
 Ro_b = 0.1
@@ -25,22 +25,25 @@ averaged_options = dict(unique_times=False, load=False, get_grid=False,
                         open_dataset_kwargs=dict(chunks="auto"))
 
 # Variables to load
-variables_xz = ["∫⁵εₖdy", "∫¹⁰εₖdy", "∫⁵εₚdy", "∫¹⁰εₚdy"]
-variables_xy = ["⟨ε̄ₖ⟩ᶻ", "⟨ε̄ₚ⟩ᶻ"]
+variables_xz = ["∫εₖdy", "∫εₚdy"]
+variables_xy = ["∫ε̄ₖdz", "∫ε̄ₚdz"]
 
 datasets = {}
 for L_value, L_key in [(L_rough, "rough"), (L_smooth, "smooth")]:
     simulation_name = f"{simname_base}_Ro_b{Ro_b}_Fr_b{Fr_b}_L{L_value}_FWHM500_dz{resolution}"
 
     xyza_dataset = open_simulation(f"{postproc_path}xyza.{simulation_name}.nc", **averaged_options)
-    aaad_dataset = open_simulation(f"{postproc_path}aaad.{simulation_name}.nc", **averaged_options).sel(buffer=buffer)
+    xyza_dataset = condense(xyza_dataset, ["∫⁵εₖdy", "∫¹⁰εₖdy"], "∫εₖdy", dimname="buffer", indices=[5, 10])
+    xyza_dataset = condense(xyza_dataset, ["∫⁵εₚdy", "∫¹⁰εₚdy"], "∫εₚdy", dimname="buffer", indices=[5, 10])
 
-    dataset = xr.merge([xyza_dataset[variables_xz], aaad_dataset[variables_xy]])
+    aaad_dataset = open_simulation(f"{postproc_path}aaad.{simulation_name}.nc", **averaged_options)
+
+    dataset = xr.merge([xyza_dataset[variables_xz], aaad_dataset[variables_xy]]).sel(buffer=buffer)
 
     # Domain restriction
     full_width_half_maximum = dataset.FWHM
-    depth = dataset.H
-    dataset = dataset.sel(z_aac=slice(buffer, 1.3*depth), x_caa=slice(-1.5*full_width_half_maximum, np.inf))
+    seamount_height = dataset.H
+    dataset = dataset.sel(z_aac=slice(buffer, 1.3*seamount_height), x_caa=slice(-1.5*full_width_half_maximum, np.inf))
 
     # Time selection
     if "time" in dataset.dims:
@@ -52,6 +55,8 @@ for L_value, L_key in [(L_rough, "rough"), (L_smooth, "smooth")]:
     # Normalize integrated dissipation
     for variable_name in variables_xz:
         dataset[f"{variable_name}_normalized"] = dataset[variable_name] / full_width_half_maximum
+    for variable_name in variables_xy:
+        dataset[f"{variable_name}_normalized"] = dataset[variable_name] / seamount_height
 
     datasets[L_key] = dataset
 
@@ -63,16 +68,11 @@ fig, axes = plt.subplots(ncols=2, nrows=4, figsize=(10, 9), gridspec_kw=dict(hsp
 #---
 
 #+++ Plot variables
-integration_bound = "⁵" if buffer == 5 else "¹⁰"
 rows = [
-    dict(var=f"∫{integration_bound}εₖdy_normalized", label=f"∫ε̄ₖdy / W [m²/s³]",
-         norm=LogNorm(vmin=2e-10, vmax=1e-7), plot_type="xz"),
-    dict(var="⟨ε̄ₖ⟩ᶻ", label="⟨ε̄ₖ⟩ᶻ",
-         norm=LogNorm(vmin=1e-10, vmax=1e-8), plot_type="xy"),
-    dict(var=f"∫{integration_bound}εₚdy_normalized", label=f"∫ε̄ₚdy / W [m²/s³]",
-         norm=LogNorm(vmin=1e-11, vmax=2e-9), plot_type="xz"),
-    dict(var="⟨ε̄ₚ⟩ᶻ", label="⟨ε̄ₚ⟩ᶻ",
-         norm=LogNorm(vmin=1e-11, vmax=1e-9), plot_type="xy")
+    dict(var="∫εₖdy_normalized", label="∫εₖdy / W [m²/s³]", norm=LogNorm(vmin=2e-10, vmax=1e-7), plot_type="xz"),
+    dict(var="∫ε̄ₖdz_normalized", label="∫ε̄ₖdz / H [m²/s³]", norm=LogNorm(vmin=2e-10, vmax=1e-7), plot_type="xy"),
+    dict(var="∫εₚdy_normalized", label="∫εₚdy / W [m²/s³]", norm=LogNorm(vmin=1e-11, vmax=2e-9), plot_type="xz"),
+    dict(var="∫ε̄ₚdz_normalized", label="∫ε̄ₚdz / H [m²/s³]", norm=LogNorm(vmin=1e-11, vmax=2e-9), plot_type="xy"),
 ]
 
 yticks = [-500, 0, 500]
